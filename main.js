64eb8b9fb8e7147f8c5ff04863e34a0dc954b58b
rijkvp <rijkvanputten@hotmail.nl>
2021-05-18 16:44:40 +0200

    Improved reloading UX

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ function insertJS(fileName, tabId) {
     // });
     // Version 2
     chrome.tabs.executeScript(tabId, {
-        file: "js/" + fileName
+        file: "js/" + fileName,
+        runAt: "document_start"
     });
 }
 
@@ -27,13 +28,30 @@ function insertCSS(fileName, tabId) {
     // });
     // Version 2
     chrome.tabs.insertCSS(tabId, {
+        file: "css/" + fileName,
+        runAt: "document_start"
+    });
+}
+
+function removeCSS(fileName, tabId) {
+    // Version 2
+    chrome.tabs.removeCSS(tabId, {
         file: "css/" + fileName
     });
 }
 
-function injectScripts(tabId) {
-    insertCSS("loading.css", tabId);
-
+function injectScripts(tabId, isReloading) {
+    if (!isReloading) {
+        // Quick fix: manually remove all CSS if not realoading
+        removeCSS("hide_explore_buttons.css");
+        removeCSS("hide_upcummies.css");
+        removeCSS("hide_notifications.css");
+        removeCSS("hide_thumbnails.css");
+        removeCSS("hide_avatars.css");
+    }
+    if (isReloading) {
+        insertCSS("loading.css", tabId);
+    }
     insertCSS("injected_styles.css", tabId);
 
     chrome.storage.sync.get(null, function (settingsData) {
@@ -66,19 +84,30 @@ function injectScripts(tabId) {
         }
     });
 }
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (tab.url.includes("www.youtube.com")) {
-        injectScripts(tabId);
+        injectScripts(tabId, true);
     }
 });
 
-chrome.storage.onChanged.addListener(function (changes, namespace) {
+function update(reloadTab) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
         currentTab = tab[0];
         if (currentTab != null && currentTab.url.includes("www.youtube.com")) {
-            chrome.tabs.reload(tab.id, function () {
-                injectScripts(currentTab.id);
-            });
+            if (reloadTab) {
+                chrome.tabs.reload(tab.id, function () {
+                    injectScripts(currentTab.id, true);
+                });
+            } else {
+                injectScripts(currentTab.id, false);
+            }
         }
     });
+}
+
+chrome.runtime.onMessage.addListener(function (request) {
+    if (request.type == "update") {
+        update(request.reloadTab);
+    }
 });
--- settings_panel/settings.js
+++ settings_panel/settings.js
@@ -7,6 +7,10 @@ const defaultSettings = {
     hide_avatars: true,
 }
 
+const reloadRequiredSettings = [
+    "block_recommendations", "block_explore_feeds"
+]
+
 function playStatusAnimation(text) {
     var status = document.getElementById('status');
     status.innerText = text;
@@ -16,12 +20,20 @@ function playStatusAnimation(text) {
     didShow = true;
 }
 
-function showUpdateStatus() {
+function onSettingsChanged(reload) {
+    // Send message to update the injected scripts/styles
+    chrome.runtime.sendMessage({type: "update", reloadTab: reload});
+    
+    // Show animation on settings panel
     let didShow = false;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
         currentTab = tab[0];
         if (currentTab != null && currentTab.url.includes("www.youtube.com")) {
-            playStatusAnimation("Reloading..");
+            if (reload) {
+                playStatusAnimation("Reloading..");
+            } else {
+                playStatusAnimation("Updating..");
+            }
         }
     });
     if (!didShow) {
@@ -29,15 +41,43 @@ function showUpdateStatus() {
     }
 }
 
-function saveSettings() {
-    let settings = {};
-    Object.keys(defaultSettings).forEach((key) => {
-        var checkbox = document.getElementById(key);
-        if (checkbox) {
-            settings[key] = checkbox.checked;
+function getChangedKeys(before, after) {
+    let changed = [];
+    Object.keys(before).forEach((key) => {
+        if (before[key] != after[key]) {
+            changed.push(key);
         }
     });
-    chrome.storage.sync.set(settings, showUpdateStatus);
+    return changed;
+}
+
+function saveSettings() {
+    chrome.storage.sync.get(null, function (settingsData) {
+        let previousSettings;
+        if (Object.entries(settingsData).length == 0) {
+            previousSettings = defaultSettings;
+        } else {
+            previousSettings = settingsData;
+        }
+
+        let settings = {};
+        Object.keys(defaultSettings).forEach((key) => {
+            var checkbox = document.getElementById(key);
+            if (checkbox) {
+                settings[key] = checkbox.checked;
+            }
+        });
+
+        let changedKeys = getChangedKeys(previousSettings, settings);
+        let reloadRequired = false;
+        changedKeys.forEach((key) => {
+            if (reloadRequiredSettings.includes(key)) {
+                reloadRequired = true;
+            }
+        });
+
+        chrome.storage.sync.set(settings, onSettingsChanged(reloadRequired));
+    });
 }
 
 
