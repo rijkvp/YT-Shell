79c3e1fe08ef1856dd7c639f695189ea1c1c0b7a
rijkvp <github@rijkvp.nl>
2021-08-12 10:20:37 +0200

    Add more explore feeds

--- js/redirect_feeds.js
+++ js/redirect_feeds.js
@@ -2,5 +2,9 @@ switch (window.location.pathname) {
     case "/feed/explore":
     case "/feed/trending":
     case "/gaming":
+    case "/channel/UClgRkhTL3_hImCAmdLfDE4g":
+    case "/channel/UCYfdidRxbB8Qhf0Nx7ioOYw":
+    case "/channel/UC4R8DWoMoI7CAwX8_LjQHig":
+    case "/channel/UCzuqhhs6NWbgTzMuM09WKDQ":
         window.location.assign("https://www.youtube.com");
 }
